e8c6d8737f5cec1bdb167344b2e5e08c97ac9d49
Kellyson Dias <kellysondias@outlook.com>
2023-12-27 16:21:01 -0300

    refactor: remove try-catch block from "start" variable

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,11 @@ import { connectDB } from "./database/connect.database";
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-	try {
-		await connectDB(`${process.env.MONGO_URI}`);
-		app.listen(port, () =>
+	app.listen(port, async () => {
+		await connectDB(`${process.env.MONGO_URI}`).then(() =>
 			console.log(`Server listening on port ${port}...`),
 		);
-	} catch (error) {
-		console.log(error);
-	}
+	});
 };
 
 start();
