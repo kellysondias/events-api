d9a34d1191124968097b707fc4fef53cc8ddc473
Kellyson Dias <kellysondias@outlook.com>
2023-12-27 14:31:32 -0300

    feat: create user schema

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,81 @@
+import Joi from "joi";
+import mongoose, { Schema } from "mongoose";
+
+const signInValidationSchema = Joi.object({
+	firstName: Joi.string().required().messages({
+		"any.required": "First name is required.",
+		"string.empty": "First name cannot be empty.",
+	}),
+	lastName: Joi.string().required().messages({
+		"any.required": "Last name is required.",
+		"string.empty": "Last name cannot be empty.",
+	}),
+	birthDate: Joi.date().iso().required().messages({
+		"any.required": "Birth date is required.",
+		"date.format": "Invalid date format.",
+	}),
+	city: Joi.string().required().messages({
+		"any.required": "City is required.",
+		"string.empty": "City cannot be empty.",
+	}),
+	country: Joi.string().required().messages({
+		"any.required": "Country is required.",
+		"string.empty": "Country cannot be empty.",
+	}),
+	email: Joi.string().email().required().messages({
+		"any.required": "Email is required.",
+		"string.empty": "Email cannot be empty.",
+		"string.email": "Invalid email address.",
+	}),
+	password: Joi.string().required().messages({
+		"any.required": "Password is required.",
+		"string.empty": "Password cannot be empty.",
+	}),
+	confirmPassword: Joi.string()
+		.valid(Joi.ref("password"))
+		.required()
+		.messages({
+			"any.required": "Confirm password is required.",
+			"any.only": "Passwords do not match.",
+		}),
+});
+
+const userSchema = new Schema({
+	firstName: {
+		type: String,
+		required: true,
+		maxlength: 255,
+	},
+	lastName: {
+		type: String,
+		required: true,
+		maxlength: 255,
+	},
+	birthDate: { type: Date, required: true },
+	city: {
+		type: String,
+		required: true,
+		maxlength: 255,
+	},
+	country: {
+		type: String,
+		required: true,
+		maxlength: 255,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		maxlength: 255,
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6,
+		maxlength: 255,
+	},
+});
+
+const UserModel = mongoose.model("User", userSchema);
+
+export { signInValidationSchema, UserModel };
