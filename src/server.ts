1afc295f4a36636029fdb4400afda8d32469df27
Kellyson Dias <kellysondias@outlook.com>
2023-12-27 15:48:59 -0300

    feat: create "server.ts"

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import app from "./app";
+import connectDB from "./database/connect.database";
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+	try {
+		await connectDB(`${process.env.MONGO_URI}`);
+		app.listen(port, () =>
+			console.log(`Server listening on port ${port}...`),
+		);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
