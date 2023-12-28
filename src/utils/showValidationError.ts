94727e450b2a0f6e0a5dc04be5db455bf6c95e4c
Kellyson Dias <kellysondias@outlook.com>
2023-12-28 11:30:42 -0300

    refactor: create the types folder

--- src/utils/showValidationError.ts
+++ src/utils/showValidationError.ts
@@ -1,22 +1,17 @@
 import { StatusCodes } from "http-status-codes";
-import { ValidationError } from "joi";
 import { Response } from "express";
-
-type ValidationResult = {
-	error: ValidationError;
-	warning?: ValidationError | undefined;
-	value: unknown;
-};
+import { ValidationResult } from "../types/validationResult";
 
 export const showValidationError = (
 	validationResult: ValidationResult,
 	res: Response,
 ) => {
+	console.log(validationResult.value);
 	return res.status(StatusCodes.BAD_REQUEST).json({
 		type: "Validation error",
 		errors: validationResult.error.details.map((error) => {
 			return {
-				resource: error.path.join("."),
+				resource: error.path.join(),
 				message: error.message,
 			};
 		}),
