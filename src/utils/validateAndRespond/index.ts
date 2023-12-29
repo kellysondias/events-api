10c060db4efbd38399f0270184fe20b56f9a1ae4
Kellyson Dias <kellysondias@outlook.com>
2023-12-29 14:50:04 -0300

    refactor: changing file name to index

---
+++ src/utils/validateAndRespond/index.ts
@@ -0,0 +1,18 @@
+import { StatusCodes } from "http-status-codes";
+import { Response } from "express";
+import { ValidationResult } from "../../types/validationResult";
+
+export const showValidationError = (
+	validationResult: ValidationResult,
+	res: Response,
+) => {
+	return res.status(StatusCodes.BAD_REQUEST).json({
+		type: "Validation error",
+		errors: validationResult.error.details.map((error) => {
+			return {
+				resource: error.path.join(),
+				message: error.message,
+			};
+		}),
+	});
+};
