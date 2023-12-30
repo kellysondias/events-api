e532926056edf98d0849ebedc5c1478495c1dbf5
Kellyson Dias <kellysondias@outlook.com>
2023-12-30 11:33:44 -0300

    refactor: add console log to internal server util function

--- src/utils/internalServerErrorMessage.ts
+++ src/utils/internalServerErrorMessage.ts
@@ -1,7 +1,9 @@
 import { Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
-export const internalServerErrorMessage = (res: Response) => {
+export const internalServerErrorMessage = (error: unknown, res: Response, ) => {
+	console.log(error);
+
 	return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
 		statusCode: 500,
 		error: "Internal Server Error",
