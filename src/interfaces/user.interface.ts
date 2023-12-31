0bdab9e194bb1ed2e58502f85d6266898e9e3a9b
Kellyson Dias <kellysondias@outlook.com>
2023-12-31 11:42:39 -0300

    refactor: update IUser import

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,6 +1,6 @@
 import { Document } from "mongoose";
 
-export default interface IUser extends Document {
+export interface IUser extends Document {
 	firstName: string;
 	lastName: string;
 	birthDate: Date;
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,5 @@
 import { model } from "mongoose";
-import IUser from "../interfaces/user.interface";
+import {IUser} from "../interfaces/user.interface";
 import { userSchema } from "../schemas/user.schema";
 
 export const UserModel = model<IUser>("User", userSchema);
