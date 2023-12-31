06671176518c6b9efd96f8578e57f8f4aa4ec49a
Kellyson Dias <kellysondias@outlook.com>
2023-12-31 11:11:52 -0300

    feat: decode user token

--- src/controllers/events/create-event.controller.ts
+++ src/controllers/events/create-event.controller.ts
@@ -1,10 +1,36 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { internalServerErrorMessage } from "../../utils/internalServerErrorMessage";
+import { unauthorizedRes } from "./unauthorized-res";
+import { verify } from "jsonwebtoken";
+import { UserModel } from "../../models/user.model";
 
-export const createEvent = (req: Request, res: Response) => {
+export const createEvent = async (req: Request, res: Response) => {
 	try {
-		res.status(StatusCodes.CREATED).json();
+		const token = req.headers.bearer!;
+
+		const decodedToken = verify(
+			token as string,
+			process.env.JWT_SECRET as string,
+		);
+
+		const user = await UserModel.findOne({
+			_id: Object(decodedToken).userId,
+		});
+
+		if (!token || !user)
+			return res
+				.status(StatusCodes.UNAUTHORIZED)
+				.json(unauthorizedRes);
+
+		const event = req.body;
+
+		const createdEvent = {
+			...event,
+			userId: user.id,
+		};
+
+		res.status(StatusCodes.CREATED).json(createdEvent);
 	} catch (error) {
 		internalServerErrorMessage(error, res);
 	}
