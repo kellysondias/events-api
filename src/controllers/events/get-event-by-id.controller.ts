31a9f663d74ca0be4f2572d6e2056475eca3e693
Kellyson Dias <kellysondias@outlook.com>
2024-01-02 17:09:53 -0300

    feat: get event by id

--- src/controllers/events/get-event-by-id.controller.ts
+++ src/controllers/events/get-event-by-id.controller.ts
@@ -1,6 +1,28 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import { errorMessage } from "../../utils/error-message";
+import { EventModel } from "../../models/event.model";
+import { internalServerErrorMessage } from "../../utils/internal-server-error-message/internal-server-error-message";
 
-export const getEventById = (req: Request, res: Response) => {
-	res.status(StatusCodes.OK).json("working");
+export const getEventById = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params;
+
+		const event = await EventModel.findById(id);
+
+		if (!event)
+			return res
+				.status(StatusCodes.NOT_FOUND)
+				.json(
+					errorMessage(
+						StatusCodes.NOT_FOUND,
+						"Not Found",
+						"Not Found",
+					),
+				);
+
+		res.status(StatusCodes.OK).json(event);
+	} catch (error) {
+		internalServerErrorMessage(error, res);
+	}
 };
