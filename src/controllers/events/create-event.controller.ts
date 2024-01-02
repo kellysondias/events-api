2c7033a935a57e3eea0fdaa1774160f8f659ac99
Kellyson Dias <kellysondias@outlook.com>
2024-01-02 11:44:13 -0300

    refactor: update create event controller

--- src/controllers/events/create-event.controller.ts
+++ src/controllers/events/create-event.controller.ts
@@ -1,9 +1,6 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { internalServerErrorMessage } from "../../utils/internal-server-error-message";
-import { unauthorizedRes } from "./unauthorized-res";
-import { verify } from "jsonwebtoken";
-import { UserModel } from "../../models/user.model";
 import { eventPayloadValidationSchema } from "../../schemas/event.schema";
 import { EventModel } from "../../models/event.model";
 import { validateAndRespond } from "../../utils/validate-and-respond/validate-and-respond";
@@ -18,27 +15,11 @@ export const createEvent = async (req: Request, res: Response) => {
 
 		if (validationError) return validationError;
 
-		const token = req.headers.bearer!;
-
-		const decodedToken = verify(
-			token as string,
-			process.env.JWT_SECRET as string,
-		);
-
-		const user = await UserModel.findOne({
-			_id: Object(decodedToken).userId,
-		});
-
-		if (!token || !user)
-			return res
-				.status(StatusCodes.UNAUTHORIZED)
-				.json(unauthorizedRes);
-
 		const payload = req.body;
 
 		const newEvent = {
 			...payload,
-			userId: user.id,
+			userId: req.user!.id,
 		};
 
 		const event = await EventModel.create(newEvent);
