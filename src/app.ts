6af34b10d70af5a730b88126b6b42ccb57a063f3
Kellyson Dias <kellysondias@outlook.com>
2024-01-02 11:39:21 -0300

    feat: create user auth middleware

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,15 @@ import "express-async-errors";
 import "dotenv/config";
 import { userRouter } from "./routes/user.route";
 import { eventsRouter } from "./routes/events.route";
+import { authenticateUser } from "./middlewares/authenticate-user.middleware";
 
 const app = express();
 
 app.use(express.json());
-app.use("/api/v1", eventsRouter);
+
 app.use("/api/v1/users", userRouter);
+
+app.use(authenticateUser);
+app.use("/api/v1", eventsRouter);
 
 export { app };
