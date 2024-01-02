0f2b4c80be1d81a9717976b58ad367c9a683a3ff
Kellyson Dias <kellysondias@outlook.com>
2024-01-02 16:05:01 -0300

    refactor: change schema types

--- src/utils/validate-and-respond/validate-and-respond.ts
+++ src/utils/validate-and-respond/validate-and-respond.ts
@@ -1,10 +1,10 @@
-import Joi from "joi";
-import { showValidationError } from "./show-schema-validation-error";
-import { Response } from "express";
+import Joi, { ObjectSchema } from "joi";
+import { showSchemaValidationError } from "./show-schema-validation-error";
+import { Request, Response } from "express";
 
 export const validateAndRespond = (
 	payload: Request | object,
-	schema: Joi.ObjectSchema,
+	schema: Joi.StringSchema<string> | ObjectSchema<unknown>,
 	res: Response,
 ) => {
 	const validationResult = schema.validate(payload, {
@@ -12,7 +12,7 @@ export const validateAndRespond = (
 	});
 
 	if (validationResult.error)
-		return showValidationError(validationResult, res);
+		return showSchemaValidationError(validationResult, res);
 
 	return null;
 };
