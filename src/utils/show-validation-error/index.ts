276151b450c7f4a929b9c7d39f8a171b968729a4
Kellyson Dias <kellysondias@outlook.com>
2024-01-03 16:59:01 -0300

    refactor: deçete show validation error function

--- src/utils/show-validation-error/index.ts
+++
@@ -1,9 +0,0 @@
-import { Response } from "express";
-import { StatusCodes } from "http-status-codes";
-
-export const showValidationError = (error: string, res: Response) => {
-	return res.status(StatusCodes.BAD_REQUEST).json({
-		type: "Validation error",
-		error,
-	});
-};
