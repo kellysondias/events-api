fd4d2ddbcfe24a1766e412afb7638df48046809a
Kellyson Dias <kellysondias@outlook.com>
2024-01-03 11:32:24 -0300

    refactor: change name to index

---
+++ src/utils/validate/show-schema-validation-error/index.ts
@@ -0,0 +1,25 @@
+import { StatusCodes } from "http-status-codes";
+import { Response } from "express";
+import { ValidationResult } from "../../../types/validation-result";
+import { Format } from "./format.class";
+
+export const showSchemaValidationError = (
+	validationResult: ValidationResult,
+	res: Response,
+) => {
+	let resource = "";
+	let message = "";
+
+	for (const error of validationResult.error.details) {
+		resource += Format.resource(resource, error.path.join());
+		message += Format.message(error.message);
+	}
+
+	return res.status(StatusCodes.BAD_REQUEST).json({
+		type: "Validation error",
+		errors: {
+			resource,
+			message,
+		},
+	});
+};
