67e33a113ba869e40d2c70c274995b7e7de13d40
Kellyson Dias <kellysondias@outlook.com>
2024-01-03 12:17:22 -0300

    refactor: update format class

--- src/utils/validate/show-schema-validation-error/format.class.ts
+++ src/utils/validate/show-schema-validation-error/format.class.ts
@@ -1,6 +1,6 @@
 export class Format {
 	static resource = (resource: string, path: (string | number)[] | string) =>
-		(resource ? ", " : "") + `'${path}'`;
+		(resource && ", ") + `'${path}'`;
 
 	static message = (message: string) => `${message} `;
 }
