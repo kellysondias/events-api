4e9085f6df5dde9fb4eafda52cb895eae19f6c77
Kellyson Dias <kellysondias@outlook.com>
2024-01-04 16:22:50 -0300

    refactor: create "delete event by id" controller class

--- src/controllers/events/delete-event-by-id.controller.ts
+++ src/controllers/events/delete-event-by-id.controller.ts
@@ -1,50 +1,47 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import * as Joi from "joi";
 import { EventModel } from "../../models/event.model";
 import { errorMessage } from "../../utils/error-message";
 import { internalServerErrorMessage } from "../../utils/internal-server-error-message";
+import { EventSchema } from "../../schemas/event.schema";
 
-const idSchema = Joi.string().length(24).hex();
+export class deleteEventByIdController {
+	static async delete(req: Request, res: Response) {
+		try {
+			const { id } = req.params;
 
-export const deleteEventById = async (
-	req: Request,
-	res: Response,
-) => {
-	try {
-		const { id } = req.params;
+			const { error } = EventSchema.id.validate(id);
 
-		const { error } = idSchema.validate(id);
+			if (error) {
+				return res
+					.status(StatusCodes.BAD_REQUEST)
+					.json(
+						errorMessage(
+							StatusCodes.BAD_REQUEST,
+							"Invalid id",
+							"Invalid id",
+						),
+					);
+			}
 
-		if (error) {
-			return res
-				.status(StatusCodes.BAD_REQUEST)
-				.json(
-					errorMessage(
-						StatusCodes.BAD_REQUEST,
-						"Invalid id",
-						"Invalid id",
-					),
-				);
+			const deletedEvent = await EventModel.findById(id);
+
+			await EventModel.findByIdAndDelete(id);
+
+			if (!deletedEvent)
+				return res
+					.status(StatusCodes.NOT_FOUND)
+					.json(
+						errorMessage(
+							StatusCodes.NOT_FOUND,
+							"Not Found",
+							"Not Found",
+						),
+					);
+
+			res.status(StatusCodes.NO_CONTENT).json("Event deleted");
+		} catch (error) {
+			internalServerErrorMessage(error, res);
 		}
-
-		const deletedEvent = await EventModel.findById(id);
-
-		await EventModel.findByIdAndDelete(id);
-
-		if (!deletedEvent)
-			return res
-				.status(StatusCodes.NOT_FOUND)
-				.json(
-					errorMessage(
-						StatusCodes.NOT_FOUND,
-						"Not Found",
-						"Not Found",
-					),
-				);
-
-		res.status(StatusCodes.NO_CONTENT).json("Event deleted");
-	} catch (error) {
-		internalServerErrorMessage(error, res);
 	}
-};
+}
