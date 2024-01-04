c3de28c070495d2915d0da689e3d0e7d0c6825d9
Kellyson Dias <kellysondias@outlook.com>
2024-01-03 21:28:08 -0300

    refactor: update event model

--- src/models/event.model.ts
+++ src/models/event.model.ts
@@ -1,5 +1,5 @@
 import { model } from "mongoose";
 import { IEvent } from "../interfaces/event.interface";
-import { eventSchema } from "../schemas/event.schema";
+import { EventSchema } from "../schemas/event.schema";
 
-export const EventModel = model<IEvent>("Event", eventSchema);
+export const EventModel = model<IEvent>("Event", EventSchema.event);
